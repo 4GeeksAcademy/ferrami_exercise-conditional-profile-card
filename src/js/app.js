506038e97535a10e3c239371653eb9d8b26e7ee6
Fernando Ramirez <86069304+ferrami@users.noreply.github.com>
2023-05-23 05:41:24 +0000

    Completed_v1

--- src/js/app.js
+++ src/js/app.js
@@ -29,14 +29,24 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  // condición ? expr1 : expr2;
+  // if (variables.name == null) {
+  //   variables.name = "Lucy";
+  // } else variables.name = variables.name;
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name == null ? "Lucy" : variables.name} ${
+    variables.lastname == null ? "Boilett" : variables.lastname
+  }</h1>
+          <h2>${variables.role == null ? "Select a role" : variables.role}</h2>
+          <h3>${variables.city == null ? "Choose a City /" : variables.city}${
+    variables.country == null ? "/ Select a country " : variables.country
+  }</h3>
+
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
